a3a4a77eaaefff03a214e0a2619919e817021d4c
Choniawko <pawel.choniawko@gmail.com>
2015-07-23 23:25:13 +0200

    sledzenie sass gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ var gulp       = require('gulp');
 var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
+gulp.task('sass', function() {
 	gulp.src('app/Resources/public/sass/**/*.scss')
 	    .pipe(sourcemaps.init())
 	    .pipe(sass())
 	    .pipe(sourcemaps.write('.'))
 	    .pipe(gulp.dest('web/css'));
 });
+
+gulp.task('watch', function(){
+	gulp.watch('app/Resources/public/sass/**/*.scss', ['sass']);
+})
+
+gulp.task('default', ['sass']);
 
