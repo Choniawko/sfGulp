b6edbe6d4154a288b4660b9fa8da7bd58ed15080
Choniawko <pawel.choniawko@gmail.com>
2015-07-24 00:21:05 +0200

    minifyCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 var gulp       = require('gulp');
 var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var minifyCSS  = require('gulp-minify-css');
+var util       = require('gulp-util');
+
+console.log(util.env.production);
+
+var config     = {
+	assetsDir: 'app/Resources/public',
+	sassPattern: 'sass/**/*.scss',
+	production: !!util.env.production
+}
 
 gulp.task('sass', function() {
-	gulp.src('app/Resources/public/sass/**/*.scss')
+	gulp.src(config.assetsDir+'/'+config.sassPattern)
 	    .pipe(sourcemaps.init())
 	    .pipe(sass())
+	    .pipe(config.production ? minifyCSS() : util.noop())
 	    .pipe(sourcemaps.write('.'))
 	    .pipe(gulp.dest('web/css'));
 });
 
 gulp.task('watch', function(){
-	gulp.watch('app/Resources/public/sass/**/*.scss', ['sass']);
+	gulp.watch(config.assetsDir+'/'+config.sassPattern, ['sass']);
 })
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'watch']);
 
