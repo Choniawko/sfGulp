06cc8f956accdeb13b3fc0a859637851a921a71f
Choniawko <pawel.choniawko@gmail.com>
2015-07-23 23:08:51 +0200

    instalacja gulp sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
 	gulp.src('app/Resources/public/sass/**/*.scss')
+	    .pipe(sourcemaps.init())
 	    .pipe(sass())
+	    .pipe(sourcemaps.write('.'))
 	    .pipe(gulp.dest('web/css'));
 });
 
