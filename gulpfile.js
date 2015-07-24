4db08851cb30899ef1920da990b1a993cf666909
Choniawko <pawel.choniawko@gmail.com>
2015-07-24 21:05:07 +0200

    sourcemap only develop

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,24 @@ var sass       = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS  = require('gulp-minify-css');
 var util       = require('gulp-util');
+var gulpif     = require('gulp-if');
 
 console.log(util.env.production);
 
 var config     = {
 	assetsDir: 'app/Resources/public',
 	sassPattern: 'sass/**/*.scss',
-	production: !!util.env.production
+	production: !!util.env.production,
+	sourceMaps: !util.env.production
 }
 
 gulp.task('sass', function() {
 	gulp.src(config.assetsDir+'/'+config.sassPattern)
+	    .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
 	    .pipe(sourcemaps.init())
 	    .pipe(sass())
 	    .pipe(config.production ? minifyCSS() : util.noop())
-	    .pipe(sourcemaps.write('.'))
+	    .pipe(gulpif(config.sourceMaps, sourcemaps.write('.')))
 	    .pipe(gulp.dest('web/css'));
 });
 
